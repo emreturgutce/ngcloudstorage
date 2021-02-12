e308fc2f16713580799481975be07095cabd3a05
emreturgutce <emrebursalee@gmail.com>
2021-02-12 12:27:56 +0300

    firebase config added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyACLR_94DrFgSb5BnNmRlwQgVzKc3W4AJc',
+    authDomain: 'angular-crud-5e572.firebaseapp.com',
+    databaseURL:
+      'https://angular-crud-5e572-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'angular-crud-5e572',
+    storageBucket: 'angular-crud-5e572.appspot.com',
+    messagingSenderId: '44382806946',
+    appId: '1:44382806946:web:27b89ce4c07ab0c7bb08fb',
+  },
 };
 
 /*
