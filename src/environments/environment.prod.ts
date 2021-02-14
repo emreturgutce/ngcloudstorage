36c3800c378ede4221d1694cf39f5a739a17a3be
emreturgutce <emrebursalee@gmail.com>
2021-02-14 15:48:11 +0300

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyACLR_94DrFgSb5BnNmRlwQgVzKc3W4AJc',
+    authDomain: 'angular-crud-5e572.firebaseapp.com',
+    databaseURL:
+      'https://angular-crud-5e572-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'angular-crud-5e572',
+    storageBucket: 'angular-crud-5e572.appspot.com',
+    messagingSenderId: '44382806946',
+    appId: '1:44382806946:web:27b89ce4c07ab0c7bb08fb',
+  },
 };
